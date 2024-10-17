e92679a2cc973bbf32999f8634d020f0613a73f6
UmarIqbal124 <123955400+UmarIqbal124@users.noreply.github.com>
2024-10-18 00:22:07 +0500

    Update my text file

--- word_count.py
+++ word_count.py
@@ -3,7 +3,7 @@ from collections import Counter
 
 # HDFS client setup
 client = InsecureClient('http://localhost:9870', user='hadoop')
-  # hdfs_path = '/user/DELLL/first_data.txt'
+  # hdfs_path = '/user/test_file.txt'
 #output_path = './result.txt'
 def word_count_from_hdfs(hdfs_path, output_path):
     # Read file from HDFS
@@ -19,8 +19,8 @@ def word_count_from_hdfs(hdfs_path, output_path):
 
 if __name__ == "__main__":
     # Specify the file in HDFS and output file
-    hdfs_input_file = '/m/data.txt'  # Path on HDFS
-    local_output_file = 'word_count_output.txt'  # Output file on local system
+    hdfs_input_file = '/user/test_file.txt'  # Path on HDFS
+    local_output_file = 'text_analysis_output.txt'  # Output file on local system
 
     # Perform word count and output results
     word_count_from_hdfs(hdfs_input_file, local_output_file)
